66d87a7046d8e8396e736ae91e7955a7793ab331
Bohdan Malish <77528085+BohdanMalish@users.noreply.github.com>
2021-06-30 14:35:57 +0300

    final fix

--- script.js
+++ script.js
@@ -1,296 +1,249 @@
-const a = document.getElementById('button_first');
+let notesListWrapper = document.querySelector(".todo-list");
+let notesArchiveListWrapper = document.querySelector(".todo-wrapper-info");
+
+const archiveAll = document.querySelector("#button-archive");
+const deleteAll = document.querySelector("#delete-button");
+const showArchive = document.querySelector("#show-archive");
+
+const showUp = document.querySelector("#show-archive-image-up");
+const showDown = document.querySelector("#show-archive-image-down");
+
+const modalEditor = document.querySelector("#Change_modal");
+const modalEditorSubmit = document.querySelector("#Change_date");
+const closeModalEditor = document.querySelector("#closeChangeMenu");
+
+const modalCreator = document.getElementById("myModal");
+const modalCreatorOpen = document.getElementById("todo-button-create");
+const closeModalCreator = document.querySelector("#close");
+const modalCreatorSumbit = document.querySelector("#InputData");
+
 let List = [];
 let ArchiveList = [];
-const Create = document.getElementById('todo-button-create');
-let todo_list = document.querySelector('.todo-list');
-let archive_list = document.querySelector('.todo-wrapper-info');
-const ArchiveAll = document.querySelector('#button-archive');
-const deleteAll = document.querySelector('#delete-button');
-let SentDate=document.querySelector('#Change_date');
-const ShowArchive = document.querySelector('#show-archive');
-let ChangeModal=document.querySelector('#Change_modal');
-let tem = JSON.parse(localStorage.getItem('list'));
-tem != null ? List = tem : List = [];
-tem = JSON.parse(localStorage.getItem('archive'));
-tem != null ? ArchiveList = tem : ArchiveList = [];
-const UploadLocal = (name, lst) => {
-    lst != undefined ? (
-        localStorage.setItem(name, JSON.stringify(lst))) : localStorage.setItem(name, JSON.stringify([]));
-    
-}
 
-const RenderItem = (td, lst, archive) => {
-    if (archive) {
-        td.innerHTML = ''
-        lst.map((i, ind) => td.innerHTML += Item(i, ind,true));
-    }
-    else {
-        td.innerHTML = '';
-        lst.map((i, ind) => td.innerHTML += Item(i, ind,false));
-    }
-    
+let localLst = JSON.parse(localStorage.getItem("list"));
+localLst != null ? (List = localLst) : (List = []);
+localLst = JSON.parse(localStorage.getItem("archive"));
+localLst != null ? (ArchiveList = localLst) : (ArchiveList = []);
 
-}
-let showUp=document.querySelector('#show-archive-image-up');
-let showDown=document.querySelector('#show-archive-image-down');
-ShowArchive.addEventListener('click', function () {
-    if (archive_list.classList[1] == 'todo-wrapper-info-unactive') {
-        archive_list.classList.remove('todo-wrapper-info-unactive');
-        showDown.style.display='none';
-        showUp.style.display='block';
-        
-    }
-    else {
-        archive_list.classList.add('todo-wrapper-info-unactive');
-        showUp.style.display='none';
-        showDown.style.display='block';
+const Item = (item, index, archive) => {
+  const archiveImg = archive
+    ? "./images/Unarchive.png"
+    : "./images/archiveblack.png";
+  const classChange = archive ? "Non-Change" : "";
+  let classIcon;
+  if (item.category == "Random Thought") {
+    classIcon = "header-line-itemRandom";
+  } else if (item.category == "Task") {
+    classIcon = "header-line-itemTask";
+  } else {
+    classIcon = "header-line-itemIdea";
+  }
+  const noteTemplate = ` <div class="todo-item ${classIcon}">
+                      
+                          <ul class="header-line item-line ">
+                              <li class="item-name ">${item?.name}</li>
+                              <li >${item.date}</li>
+                              <li>${item.category}</li>
+                              <li>${item.context}</li>
+                              <li>${item.planDate}</li>
+                          </ul>
+                              
+                              <ul class="header-line-buttons">
+                              <li><button class="Delete-but ${classChange}" onclick={modalEditorOpen(${index})}><img class="Delete_button change-button" src="./images/pen.png"></button></li>
+                              <li><button class="Delete-but" onclick={archiveNote(${index},${archive})}><img class="Delete_button" src=${archiveImg}></button> </li>
+                              <li><button class="Delete-but" onclick={deleteNote(${archive},${index})}><img class="Delete_button" src="./images/garbageBlack.png"></img></button></li>
+                          </ul>
+          
+                      
+                  </div>`;
+  return noteTemplate;
+};
 
-    }
+const uploadLocal = (name, lst) => {
+  lst
+    ? localStorage.setItem(name, JSON.stringify(lst))
+    : localStorage.setItem(name, JSON.stringify([]));
+};
 
+const renderNotes = (td, lst, archive) => {
+  if (archive) {
+    td.innerHTML = "";
+    lst.map((i, ind) => (td.innerHTML += Item(i, ind, true)));
+  } else {
+    td.innerHTML = "";
+    lst.map((i, ind) => (td.innerHTML += Item(i, ind, false)));
+  }
+};
+
+showArchive.addEventListener("click", () => {
+  if (notesArchiveListWrapper.classList[1] == "todo-wrapper-info-unactive") {
+    notesArchiveListWrapper.classList.remove("todo-wrapper-info-unactive");
+    showDown.style.display = "none";
+    showUp.style.display = "block";
+  } else {
+    notesArchiveListWrapper.classList.add("todo-wrapper-info-unactive");
+    showUp.style.display = "none";
+    showDown.style.display = "block";
+  }
 });
 
-ArchiveAll.addEventListener('click', function () {
-    List.map((i) => { ArchiveList.push(i); });
-    List = [];
-    RenderItem(archive_list, ArchiveList, true);
-    RenderItem(todo_list, List, false);
-    UploadLocal('archive',ArchiveList);
-    UploadLocal('list', List);
-    RenderStats(List,ArchiveList);
-   
+archiveAll.addEventListener("click", () => {
+  List.forEach((i) => {
+    ArchiveList.push(i);
+  });
+  List = [];
+  renderNotes(notesArchiveListWrapper, ArchiveList, true);
+  renderNotes(notesListWrapper, List, false);
+  uploadLocal("archive", ArchiveList);
+  uploadLocal("list", List);
+  renderStats(List, ArchiveList);
 });
 
-deleteAll.addEventListener('click', function () {
-    List = [];
-    todo_list.innerHTML = '';
-    ArchiveList=[];
-    archive_list.innerHTML='';
-    UploadLocal('list',List);
-    UploadLocal('archive',ArchiveList);
-    RenderStats(List,ArchiveList);
-
+deleteAll.addEventListener("click", () => {
+  List = [];
+  notesListWrapper.innerHTML = "";
+  ArchiveList = [];
+  notesArchiveListWrapper.innerHTML = "";
+  uploadLocal("list", List);
+  uploadLocal("archive", ArchiveList);
+  renderStats(List, ArchiveList);
 });
 
+const deleteNote = (archiv, index) => {
+  if (archiv) {
+    ArchiveList.pop(index);
+    renderNotes(notesArchiveListWrapper, ArchiveList, archiv);
+    uploadLocal("archive", ArchiveList);
+  } else {
+    List.pop(index);
+    renderNotes(notesListWrapper, List, archiv);
+    uploadLocal("list", List);
+  }
+  renderStats(List, ArchiveList);
+};
 
-const Delete = (archiv, index) => {
-    if (archiv) {
-        ArchiveList.pop(index);
-        RenderItem(archive_list, ArchiveList, archiv);
-        UploadLocal('archive', ArchiveList);
-    }
-    else {
-        List.pop(index);
-        RenderItem(todo_list, List, archiv);
-        UploadLocal('list', List);
-    }
-    RenderStats(List,ArchiveList);
-
-
-}
-const Item = (item,index,archive) => {
-    
-    let img;
-    let classChange='';
-    let archiv;
-    if(archive){
-        img='Unarchive.png';
-        classChange='Non-Change';
-        
-    }
-    else{
-        img='archiveblack.png';
-    }
-    let cl;
-    if(item.category=='Random Thought'){
-        cl='header-line-itemRandom';
-    }
-    else if(item.category=='Task'){
-        cl='header-line-itemTask';
-    }
-    else{
-        cl='header-line-itemIdea';
-    }
-    let temp = ` <div class="todo-item ${cl}">
-                    
-                        <ul class="header-line item-line ">
-                            <li class="item-name ">${item?.name}</li>
-                            <li >${item.date}</li>
-                            <li>${item.category}</li>
-                            <li>${item.context}</li>
-                            <li>${item.planDate}</li>
-                        </ul>
-                            
-                            <ul class="header-line-buttons">
-                            <li><button class="Delete-but ${classChange}" onclick={Change(${index})}><img class="Delete_button change-button" src="pen.png"></button></li>
-                            <li><button class="Delete-but" onclick={Archive(${index},${archive})}><img class="Delete_button" src=${img}></button> </li>
-                            <li><button class="Delete-but" onclick={Delete(${archiv},${index})}><img class="Delete_button" src="garbageBlack.png"></img></button></li>
-                        </ul>
-        
-                    
-                </div>`;
-    return temp;
-}
-
-
-
-
-const Archive = (index,archive) => {
-    if(archive){
-        List.push(ArchiveList[index]);
-        ArchiveList.pop(index);
-        RenderItem(todo_list,List,false);
-        RenderItem(archive_list,ArchiveList,true);
-        UploadLocal('list', List);
-        UploadLocal('archive', ArchiveList);
-        UploadLocal('list', List);  
-    RenderStats(List,ArchiveList);
-
-    }
-    else{
+const archiveNote = (index, archive) => {
+  if (archive) {
+    List.push(ArchiveList[index]);
+    ArchiveList.pop(index);
+    renderNotes(notesListWrapper, List, false);
+    renderNotes(notesArchiveListWrapper, ArchiveList, true);
+    uploadLocal("list", List);
+    uploadLocal("archive", ArchiveList);
+    uploadLocal("list", List);
+    renderStats(List, ArchiveList);
+  } else {
     ArchiveList.push(List[index]);
     List.pop(index);
-    RenderItem(todo_list,List,false);
-    RenderItem(archive_list,ArchiveList,true);
-    UploadLocal('list', List);
-    UploadLocal('archive', ArchiveList);
-    RenderStats(List,ArchiveList);
+    renderNotes(notesListWrapper, List, false);
+    renderNotes(notesArchiveListWrapper, ArchiveList, true);
+    uploadLocal("list", List);
+    uploadLocal("archive", ArchiveList);
+    renderStats(List, ArchiveList);
+  }
+};
 
-}}
-
-
-
-
-
-
-let SentForm = document.querySelector('#InputData');
-let CloseForm = document.querySelector('#close');
-let closeFormChange=document.querySelector('#closeChangeMenu');
-closeFormChange.addEventListener('click',function(){
-    ChangeModal.style.display="none";
-})
-CloseForm.addEventListener('click', function () {
-    modal.style.display = "none";
+closeModalEditor.addEventListener("click", () => {
+  modalEditor.style.display = "none";
 });
-SentForm.addEventListener('submit', function (e) {
-    e.preventDefault();
-    let tempDate=new Date;
-    let datmas=[]
-    datmas.push(e.target.date.value);
-    let Note = {
-        name: e.target.name.value,
-        date: tempDate.toLocaleDateString(),
-        category: e.target.category.value,
-        context: e.target.context.value,
-        planDate:datmas
-    }
-    List.push(Note);
-    modal.style.display = "none";
-    todo_list.innerHTML = '';
-    
-    List.map((i, index) => todo_list.innerHTML += Item(i, index,false));
-    localStorage.setItem('list', JSON.stringify(List));
-    RenderStats(List,ArchiveList);
-
-
+closeModalCreator.addEventListener("click", () => {
+  modalCreator.style.display = "none";
 });
-const SubmitD=(e,index)=>{
-e.preventDefault();
-let newDate=e.target.date.value;
-let temp=List[index].planDate;
-newDate==''?ChangeModal.style.display="none":(
-temp.push(newDate));
-List[index].planDate=temp;
-RenderItem(todo_list, List, false);
-UploadLocal('list', List);
-ChangeModal.style.display="none";
-}
-const Change=(index)=>{
-ChangeModal.style.display="block";
-SentDate.addEventListener('submit',(e)=>SubmitD(e,index));
-}
-const ModalON=()=>{
-    modal.style.display = "block";
-}
-var modal = document.getElementById('myModal');
-Create.onclick = function () {
-    modal.style.display = "block";
-}
+
+modalCreatorSumbit.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let tempDate = new Date();
+  const Note = {
+    name: e.target.name.value,
+    date: tempDate.toLocaleDateString(),
+    category: e.target.category.value,
+    context: e.target.context.value,
+    planDate: e.target.date.value,
+  };
+  List.push(Note);
+  modalCreator.style.display = "none";
+  notesListWrapper.innerHTML = "";
+  List.forEach(
+    (i, index) => (notesListWrapper.innerHTML += Item(i, index, false))
+  );
+  localStorage.setItem("list", JSON.stringify(List));
+  renderStats(List, ArchiveList);
+});
+const modalEditorFunc = (e, index) => {
+  e.preventDefault();
+  let newDate = e.target.date.value;
+  newDate == ""
+    ? (modalEditor.style.display = "none")
+    : (List[index].planDate = e.target.date.value);
+  renderNotes(notesListWrapper, List, false);
+  uploadLocal("list", List);
+  modalEditor.style.display = "none";
+};
+const modalEditorOpen = (index) => {
+  modalEditor.style.display = "block";
+  modalEditorSubmit.addEventListener("submit", (e) =>
+    modalEditorFunc(e, index)
+  );
+};
+
+modalCreatorOpen.onclick = function () {
+  modalCreator.style.display = "block";
+};
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+  if (event.target == modalCreator) {
+    modalCreator.style.display = "none";
+  }
+};
 window.onclick = function (event) {
-    if (event.target == ChangeModal) {
-        ChangeModal.style.display = "none";
+  if (event.target == modalEditor) {
+    modalEditor.style.display = "none";
+  }
+};
+
+const renderStats = (List, ArchiveList) => {
+  let randAct = document.querySelector("#Random-active");
+  let randArc = document.querySelector("#Random-archive");
+  let taskAct = document.querySelector("#Task-active");
+  let taskArc = document.querySelector("#Task-archive");
+  let ideact = document.querySelector("#Idea-active");
+  let ideakArc = document.querySelector("#Idea-archive");
+  let tempId = 0;
+  let tempTask = 0;
+  let tempRand = 0;
+  List.map((i) => {
+    if (i.category == "Idea") {
+      tempId++;
+    } else if (i.category == "Task") {
+      tempTask++;
+    } else if (i.category == "Random Thought") {
+      tempRand++;
     }
-}
-
-
-
-List.map((i, index) => todo_list.innerHTML += Item(i, index,false));
-ArchiveList.map((i, index) => archive_list.innerHTML +=Item(i, index,true));
-
-
-
-
-const RenderStats=(List,ArchiveList)=>{
-let randAct=document.querySelector('#Random-active');
-let randArc=document.querySelector('#Random-archive');
-let taskAct=document.querySelector('#Task-active');
-let taskArc=document.querySelector('#Task-archive');
-let ideact=document.querySelector('#Idea-active');
-let ideakArc=document.querySelector('#Idea-archive');
-let tempId=0;
-let tempTask=0;
-let tempRand=0;
-List.map((i)=>{
-    if(i.category=='Idea'){
-        tempId++;
-      
+  });
+  ideact.innerHTML = tempId;
+  taskAct.innerHTML = tempTask;
+  randAct.innerHTML = tempRand;
+  tempId = 0;
+  tempTask = 0;
+  tempRand = 0;
+  ArchiveList.map((i) => {
+    if (i.category == "Idea") {
+      tempId++;
+    } else if (i.category == "Task") {
+      tempTask++;
+    } else if (i.category == "Random Thought") {
+      tempRand++;
     }
-    else if(i.category=='Task'){
-        tempTask++;
-     
-    }
-    else if(i.category=='Random Thought'){
-        tempRand++;
-       
-    }
-    else{
+  });
+  ideakArc.innerHTML = tempId;
+  taskArc.innerHTML = tempTask;
+  randArc.innerHTML = tempRand;
+};
 
-    }
-});
-ideact.innerHTML=tempId;
-taskAct.innerHTML=tempTask;
-randAct.innerHTML=tempRand;
-tempId=0;
-tempTask=0;
-tempRand=0;
-ArchiveList.map((i)=>{
-    if(i.category=='Idea'){
-        tempId++;
-     
-    }
-    else if(i.category=='Task'){
-        tempTask++;
-    
-    }
-    else if(i.category=='Random Thought'){
-        tempRand++;
-      
-    }
-    else{
+List.forEach(
+  (i, index) => (notesListWrapper.innerHTML += Item(i, index, false))
+);
+ArchiveList.forEach(
+  (i, index) => (notesArchiveListWrapper.innerHTML += Item(i, index, true))
+);
 
-    }
-});
-ideakArc.innerHTML=tempId;
-taskArc.innerHTML=tempTask;
-randArc.innerHTML=tempRand;
-}
-
-RenderStats(List,ArchiveList);
-
-
-
-
+renderStats(List, ArchiveList);
